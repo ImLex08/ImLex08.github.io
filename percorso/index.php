02a42d9f44b28d5ae960f7648fe9e337e6c3164a
ImLex08 <lex.lorenzoippolito08@gmail.com>
2025-07-20 19:06:25 +0200

    added home

--- percorso/index.php
+++ percorso/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Percorso</title>
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="../stylesheet.css">
 </head>
 <body>
    <?php include('header.php'); ?>
